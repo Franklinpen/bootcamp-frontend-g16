69382eb33e0d39f4333030283be81592ad6af857
Franklin Peña <franklin.pena@tecsup.edu.pe>
2023-12-06 20:04:16 -0500

    Semana11, iniciando con react router DOM

---
+++ semana09/dia03-react-todo-app/src/App.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react"
+
+const App = () => {
+  const DEFAULT_TODOS = [
+    {
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": true
+    },
+    {
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+  const [input, setInput] = useState('')
+
+  const handleChange = (event) => {
+    // Estamos capturando lo que escribimos en la caja de texto
+    // console.log(event.target.value)
+    const value = event.target.value
+
+    setInput(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log('Estoy enviando el formulario...')
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed : false
+    }
+
+    setTodos([...todos, newTodo])
+
+    setInput('')
+  }
+
+  return (
+    <main
+      className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg shadow-lg p-4"
+    >
+      <h1 className="text-2xl font-bold">TODO APP</h1>
+
+      {/* {JSON.stringify(todos)} */}
+
+      {/* {input} */}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="w-full border my-3 p-3"
+          type="text"
+          placeholder="¿Qué deseas hacer hoy?"
+          required
+          onChange={handleChange}
+          value={input}
+        />
+      </form>
+
+      <div className="flex justify-between">
+        <span className="font-bold">3 de 8</span>
+        <button
+          className="bg-blue-500 rounded-lg px-2 py-1 text-white hover:bg-blue-700 duration-300"
+        >
+          Limpiar tareas completadas
+        </button>
+      </div>
+
+      <section className="mt-4">
+        <ul className="flex flex-col gap-2">
+          {todos.map(todo => {
+            return (
+              <li className="flex" key={todo.id}>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                />
+                <div className="w-full flex justify-between items-center">
+                  <div className="">
+                    {todo.title}
+                    {/*{todo.id}*/}
+                  </div>
+                  <button
+                    className="bg-red-300 rounded-lg px-2 py-2"
+                  >
+                    ❌
+                  </button>
+                </div>
+              </li>
+            )
+          })}
+          
+        </ul>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ semana11/dia01-react-router/src/App.jsx
@@ -0,0 +1,28 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import { BrowserRouter, Routes, Route, Link  } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>React router DOM - Components</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+        </main>
+    </BrowserRouter>
+  )
+}
